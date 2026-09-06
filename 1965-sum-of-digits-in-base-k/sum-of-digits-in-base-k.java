016b173c2288aa65588115f8544658bbeb8016ca
AUAayush09 <145000609+AUAayush09@users.noreply.github.com>
2026-09-07 00:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.14%) - LeetSync

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
@@ -1,15 +1,11 @@
 class Solution {
     public int sumBase(int n, int k) {
-        int sum = 0 , rev = 0;
+        int sum = 0;
         while(n>0){
             int rem = n%k;
-            rev = 10 * rev + rem;
+            sum = sum + rem;
             n/=k;
-        } while(rev>0){
-            int digit = rev%10;
-            sum = sum + digit;
-            rev/=10;
-        }
+        } 
         return sum;
     }
 }
